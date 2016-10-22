6f132fe42f7393f20d6b5674ebd47366b077f30b
pointatnick <nicholashma@gmail.com>
2016-10-22 16:53:11 -0700

    Set up "Create Account" functionality for create account page

--- ReThrift/app/src/main/java/rethrift/rethrift/CreateAccountActivity.java
+++ ReThrift/app/src/main/java/rethrift/rethrift/CreateAccountActivity.java
@@ -2,6 +2,7 @@ package rethrift.rethrift;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class CreateAccountActivity extends AppCompatActivity {
 
@@ -9,5 +10,9 @@ public class CreateAccountActivity extends AppCompatActivity {
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_create_account);
+  }
+
+  public void createAcct(View view) {
+    // send the user account info
   }
 }
