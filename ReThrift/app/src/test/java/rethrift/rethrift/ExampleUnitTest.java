047d8cfd70e1ce7257a7a3ac502eadfd48e37b6c
meeshic <michellecsr@ucla.edu>
2016-10-22 16:08:42 -0700

    hi, testtest

--- ReThrift/app/src/test/java/rethrift/rethrift/ExampleUnitTest.java
+++ ReThrift/app/src/test/java/rethrift/rethrift/ExampleUnitTest.java
@@ -9,6 +9,9 @@ import static org.junit.Assert.*;
  *
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
+
+//hello, I made a comment here.
+
 public class ExampleUnitTest {
   @Test
   public void addition_isCorrect() throws Exception {
