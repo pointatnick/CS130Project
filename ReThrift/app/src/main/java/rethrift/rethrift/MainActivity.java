a5efc04734ded5a7d09c1d72c580cd05c147f91f
pointatnick <nicholashma@gmail.com>
2016-10-28 10:28:45 -0700

    Connect activity_main.xml to MainActivity

--- ReThrift/app/src/main/java/rethrift/rethrift/MainActivity.java
+++ ReThrift/app/src/main/java/rethrift/rethrift/MainActivity.java
@@ -1,8 +1,17 @@
 package rethrift.rethrift;
 
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
 /**
  * Created by nhma on 10/28/16.
  */
 
-public class MainActivity {
+public class MainActivity extends AppCompatActivity {
+
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+  }
 }
