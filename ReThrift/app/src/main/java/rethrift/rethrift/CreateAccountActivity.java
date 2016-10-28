fd8df7f420adfb23cf8a1f3393e3bb7beb75fd1a
pointatnick <nicholashma@gmail.com>
2016-10-28 11:35:34 -0700

    Added TODOs

--- ReThrift/app/src/main/java/rethrift/rethrift/CreateAccountActivity.java
+++ ReThrift/app/src/main/java/rethrift/rethrift/CreateAccountActivity.java
@@ -10,6 +10,7 @@ import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
+import android.view.Window;
 import android.widget.EditText;
 
 import org.json.JSONException;
@@ -29,6 +30,7 @@ public class CreateAccountActivity extends AppCompatActivity {
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
+    requestWindowFeature(Window.FEATURE_NO_TITLE);
     setContentView(R.layout.activity_create_account);
 
     // bind EditText fields
--- ReThrift/app/src/main/java/rethrift/rethrift/MainActivity.java
+++ ReThrift/app/src/main/java/rethrift/rethrift/MainActivity.java
@@ -3,10 +3,6 @@ package rethrift.rethrift;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
-/**
- * Created by nhma on 10/28/16.
- */
-
 public class MainActivity extends AppCompatActivity {
 
   @Override
@@ -14,4 +10,8 @@ public class MainActivity extends AppCompatActivity {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
   }
+
+  // TODO: profile preview (left screen)
+
+  // TODO: search (right screen)
 }
