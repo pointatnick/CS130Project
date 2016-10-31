ce038357b26658c97456b4caff75981d6784b755
pointatnick <nicholashma@gmail.com>
2016-10-30 20:29:48 -0700

    Renamed salesboard.xml to activity_salesboard.xml

--- ReThrift/app/src/main/java/rethrift/rethrift/SalesboardActivity.java
+++ ReThrift/app/src/main/java/rethrift/rethrift/SalesboardActivity.java
@@ -17,7 +17,7 @@ public class SalesboardActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
-        setContentView(R.layout.salesboard);
+        setContentView(R.layout.activity_salesboard);
     }
 
     public void createPost(View view){
