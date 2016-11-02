044791d279efb34735e34ebb3f06fba0cc7f4704
pointatnick <nicholashma@gmail.com>
2016-11-02 01:47:06 -0700

    Renamed create_post.xml to activity_create_post.xml

--- ReThrift/app/src/main/java/rethrift/rethrift/CreatePostActivity.java
+++ ReThrift/app/src/main/java/rethrift/rethrift/CreatePostActivity.java
@@ -21,7 +21,7 @@ public class CreatePostActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
-        setContentView(R.layout.create_post);
+        setContentView(R.layout.activity_create_post);
 
         title = (EditText) findViewById(R.id.title_field);
         price = (EditText) findViewById(R.id.price_field);
