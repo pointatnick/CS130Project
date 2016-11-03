1a31eb123094d429b163ba0be68d9e70d644f985
taegen <taegen0508@Hotmail.com>
2016-11-02 20:39:01 -0700

    added drawer

--- ReThrift/app/src/main/java/rethrift/rethrift/SalesboardActivity.java
+++ ReThrift/app/src/main/java/rethrift/rethrift/SalesboardActivity.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.view.Window;
+import android.widget.ListView;
+import android.widget.ArrayAdapter;
 
 
 /**
@@ -13,19 +15,33 @@ import android.view.Window;
 
 public class SalesboardActivity extends AppCompatActivity {
 
+    //added
+    private ListView mDrawerList;
+    private ArrayAdapter<String> mAdapter;
+    //@@@@@@
+
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_salesboard);
+
+      //  mDrawerList = (ListView)findViewById(R.id.navList);
+     //   addDrawerItems();
     }
+     //added
+     //  private void addDrawerItems() {
+     //    String[] items = { "Name", "E-Mail", "Phone Number", "Profile"};
+     //   mAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, items);
+     //   mDrawerList.setAdapter(mAdapter);
+     //}
+    //@@@@@
 
     public void createPost(View view){
         Intent intent = new Intent(this, CreatePostActivity.class);
         startActivity(intent);
     }
 
-    // TODO: profile preview (left screen)
 
     // TODO: search (right screen)
 
