7a94b2798b5d018a415180c223bff3c762dc2b22
taegen <taegen0508@Hotmail.com>
2016-11-20 20:06:18 -0800

    linked watchlist

--- ReThrift/app/src/main/java/rethrift/rethrift/WatchListActivity.java
+++ ReThrift/app/src/main/java/rethrift/rethrift/WatchListActivity.java
@@ -2,15 +2,30 @@ package rethrift.rethrift;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+import android.view.View;
 
 /**
  * Created by MC on 11/17/2016.
  */
 
 public class WatchListActivity extends AppCompatActivity {
+    private TextView Title, Price, Location;
     @Override
-    protected void onCreate(Bundle savedInstanceState){
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.watchlist);
+        setContentView(R.layout.card_preview_layout);
+
+        Bundle extras = getIntent().getExtras();
+
+            Title = (TextView) findViewById(R.id.title);
+            Price = (TextView) findViewById(R.id.price);
+            Location = (TextView) findViewById(R.id.location);
+
+        if (extras != null) {
+            Price.setText(extras.getString("PRICE"));
+            Location.setText(extras.getString("LOCATION"));
+            Title.setText(extras.getString("TITLE"));
+        }
     }
 }
--- ReThrift/app/src/test/java/rethrift/rethrift/ExampleUnitTest.java
+++ ReThrift/app/src/test/java/rethrift/rethrift/ExampleUnitTest.java
@@ -1,5 +1,5 @@
 package rethrift.rethrift;
-
+import junit.framework.*;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -12,9 +12,69 @@ import static org.junit.Assert.*;
 
 //hello, I made a comment here. (:
 
-public class ExampleUnitTest {
-  @Test
-  public void addition_isCorrect() throws Exception {
-    assertEquals(4, 2 + 2);
-  }
+public class ExampleUnitTest extends TestCase {
+  protected String username, password;
+  protected String firstName, lastName, email, phone_number, username_create, password_create, verifyPassword;
+  // assigning the values
 }
+ /* protected void setUp(){
+    username = "taegen";
+    password = "123123";
+  }
+  public boolean lettersOnly(String str)
+  {}
+
+  public boolean numbersOnly(String str)
+  {}
+
+  public boolean pn_format(String phone_number)
+  {}
+
+  public boolean usernameLength(String str)
+  {
+  }
+
+  public boolean passwordLength(String str)
+  {
+
+  }
+  @Test
+  public void usernameIsCorrect() throws Exception {
+    //check letteresonly AND length of username
+    assertTrue(lettersOnly(username));
+  }
+
+  @Test
+  public void passwordIsCorrect() throws Exception {
+    //check lettersonly AND length of password
+    assertTrue(lettersOnly(password));
+
+  }
+
+  @Test
+  public void firstNameIsCorrect() throws Exception {
+    assertTrue(lettersOnly(firstName));
+  }
+
+  @Test
+  public void lastNameIsCorrect() throws Exception {
+    assertTrue(lettersOnly(lastName));
+  }
+  @Test
+  public void phone_numberIsCorrect() throws Exception {
+    assertTrue(numbersOnly(phone_number) && pn_format(phone_number));
+  }
+  @Test
+  public void username_createIsCorrect() throws Exception {
+    assertTrue(lettersOnly(username_create) && usernameLength(username_create));
+  }
+  @Test
+  public void password_createIsCorrect() throws Exception {
+    assertTrue(lettersOnly(password_create) && passwordLength(password_create));
+  }
+  @Test
+ // public void verifyPasswordIsCorrect() throws Exception {
+ //   assertEquals(password_create == verifyPassword);
+ // }
+
+}*/
